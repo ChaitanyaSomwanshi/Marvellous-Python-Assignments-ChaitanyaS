05cef7ea5c0a1452d2b90959e63004d40a252aa4
ChaitanyaSomwanshi <Chaitanya.somwanshi1392@gmail.com>
2025-06-08 10:24:01 +0530

    Add files via upload

--- Assignment_14/Assignment14_9.py
+++ Assignment_14/Assignment14_9.py
@@ -10,4 +10,22 @@ class Product:
         self.name = name
         self.price = price
 
+    def __eq__ (self,Other):
+        if isinstance(Other,Product):
+            if Other.price == self.price:
+                return True
+
+        return False
+
+
+def main():
+    obj1 = Product("Oil",15)
+    obj2 = Product("BabyOil",15)
+
+    print( obj1 == obj2)
+
+if __name__ == "__main__":
+    main()
+
+
         
